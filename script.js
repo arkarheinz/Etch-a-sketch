954aa74005f2193c6628fd4d952b37c505bfadf6
Arkar Hein <arkarhein14314@gmail.com>
2024-06-25 11:26:42 +0700

    Add function to make user Choose map size

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 // create parent element
 const container = document.querySelector("#container");
 
+// Map div
+const map = document.createElement("div");
+map.setAttribute("id", "map");
+container.appendChild(map);
+
+
+// set userInput
+let userInput =  parseInt(prompt("Choose your map size! Choose between 1 to 100!"));
+console.log(userInput)
+
 // Lopping for 16 squares in a row
 const createRow = () => {
 
     // div to put grid in a row 
     const gridRow = document.createElement("div");
     gridRow.setAttribute("class", "gridRow");
-    container.appendChild(gridRow);
+    map.appendChild(gridRow);
 
-    for (let i = 1; i <= 16; i++) {
+    for (let i = 1; i <= userInput; i++) {
         const grid = document.createElement("div");
         grid.setAttribute("class", "grid");
         gridRow.appendChild(grid);
@@ -18,7 +28,7 @@ const createRow = () => {
 
 // creatingColumn
 const createColumn = () => {
-    for (let j = 1; j <=16; j++) {
+    for (let j = 1; j <= userInput; j++) {
         createRow();
     }
 }
@@ -27,5 +37,17 @@ const createGrid = () => {
     createRow();
     createColumn();
 }
-createGrid();
 
+const createMap = () => {
+    if (userInput >= 1 && userInput <= 100) {
+        createGrid();
+    } else if (userInput >= 1 && userInput >= 100) {
+        alert("You choose the wrong number!")
+        userInput = prompt("Please choose again between 1 to 100!")
+    } else {
+        userInput = 16;
+        createGrid();
+    }
+}
+
+createMap();
